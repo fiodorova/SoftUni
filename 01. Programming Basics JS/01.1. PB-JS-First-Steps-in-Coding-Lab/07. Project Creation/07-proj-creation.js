1f0987c3b30ac9ad1c6aa1d2e0377874cf00e7bd
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-10-17 09:27:30 +0300

    Update 07-proj-creation.js

--- 01. Programming Basics JS/01.1. PB-JS-First-Steps-in-Coding-Lab/07. Project Creation/07-proj-creation.js
+++ 01. Programming Basics JS/01.1. PB-JS-First-Steps-in-Coding-Lab/07. Project Creation/07-proj-creation.js
@@ -1,11 +1,10 @@
 function projCreation(input){
     let architect = input[0];
-    let proj = Number(input[1])
+    let proj = Number(input[1]);
     let hours = proj * 3;
 
-    console.log(`The architect ${architect} will need ${hours} hours to complete ${proj} project/s.`)
+    console.log(`The architect ${architect} will need ${hours} hours to complete ${proj} project/s.`);
 }
 
-projCreation(["George ",
-"4 "]
-)
+projCreation(["George",
+"4 "])
