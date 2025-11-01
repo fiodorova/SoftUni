27a61a68ee2d58275581b4afbeac1247ebb892dc
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-11-01 09:20:37 +0200

    Add files via upload

---
+++ 01. Programming Basics JS/03.1. PB-JS-Conditional-Statements-Advanced-Lab/05. Small Shop/05-smallShop.js
@@ -0,0 +1,43 @@
+function smallShop(input) {
+    let product = input[0];
+    let town = input[1];
+    let quantity = Number(input[2]);
+    let result = 0;
+
+    switch (town) {
+        case "Sofia":
+            switch (product) {
+                case "coffee": result = quantity * 0.5; break;
+                case "water": result = quantity * 0.8; break;
+                case "beer": result = quantity * 1.2; break;
+                case "sweets": result = quantity * 1.45; break;
+                case "peanuts": result = quantity * 1.6; break;
+            } 
+            break;
+        case "Plovdiv":
+            switch (product) {
+                case "coffee": result = quantity * 0.4; break;
+                case "water": result = quantity * 0.7; break;
+                case "beer": result = quantity * 1.15; break;
+                case "sweets": result = quantity * 1.3; break;
+                case "peanuts": result = quantity * 1.5; break;
+            }
+            break;
+        case "Varna":
+            switch (product) {
+                case "coffee": result = quantity * 0.45; break;
+                case "water": result = quantity * 0.7; break;
+                case "beer": result = quantity * 1.10; break;
+                case "sweets": result = quantity * 1.35; break;
+                case "peanuts": result = quantity * 1.55; break;
+            }
+            break;
+    }
+    console.log(result)
+}
+
+smallShop(["sweets",
+"Sofia",
+"2.23"])
+
+
