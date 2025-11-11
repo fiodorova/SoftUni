875bd396409d22a13a4541821e33db261fd2dedf
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-11-11 08:33:06 +0200

    Update 08-numbersDivisibleBy9.js

--- 01. Programming Basics JS/04.1. PB-JS-For-Loop-Lab/08. Numbers Divisible By 9/08-numbersDivisibleBy9.js
+++ 01. Programming Basics JS/04.1. PB-JS-For-Loop-Lab/08. Numbers Divisible By 9/08-numbersDivisibleBy9.js
@@ -2,28 +2,6 @@ function numbersDivisibleBy9(input) {
     let startNumber = Number(input[0]);
     let endNumber = Number(input[1]);
     let sum = 0;
-
-    for(let i = startNumber; i <= endNumber; i++){
-        if(i % 9 === 0){
-            sum += i
-        }
-    }
-    console.log(`The sum: ${sum}`);
-
-    for(let i = startNumber; i <= endNumber; i++){
-        if(i % 9 === 0){
-            console.log(i)
-        }
-    }   
-}
-
-numbersDivisibleBy9(["100", "200"])
-
-
-function numbersDivisibleBy9(input) {
-    let startNumber = Number(input[0]);
-    let endNumber = Number(input[1]);
-    let sum = 0;
     let res = ""
 
     for(let i = startNumber; i <= endNumber; i++){
@@ -37,4 +15,5 @@ function numbersDivisibleBy9(input) {
 }
 
 numbersDivisibleBy9(["100", "200"])
+
 
