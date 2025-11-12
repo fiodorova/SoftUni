e67809a7b1ab4b99ec11fca6e02f51e8d1bbd5d5
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-11-12 06:55:20 +0200

    Add files via upload

---
+++ 01. Programming Basics JS/04.2. PB-JS-For-Loop-Exercise/02. Multiplication Table/02-multiplicationTable.js
@@ -0,0 +1,9 @@
+function multiplicationTable(input) {
+    let number = Number(input[0]);
+
+    for(let a = 1; a <= 10; a++){
+        console.log(`${a} * ${number} = ${a * number}`)
+    }
+}
+
+multiplicationTable(["10"])
