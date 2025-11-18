d2f2849a6f142d8ee9ecc7df5324db2095e496c1
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-11-18 07:35:25 +0200

    Add files via upload

---
+++ 01. Programming Basics JS/05.1. PB-JS-While-Loop-Lab/08. Graduation/08-graduation.js
@@ -0,0 +1,45 @@
+function graduation(input) {
+    let index = 0;
+    let name = input[index];
+    index++;
+
+    let i = 1;
+    let badGradeCounter = 0;
+    let sumGrade = 0;
+    let isExcluded = false;
+
+    while(i <= 12){
+        let tempGrade = Number(input[index]);
+        index++;
+
+        if(tempGrade < 4) {
+            badGradeCounter++;
+            if(badGradeCounter >= 2) {
+                isExcluded = true;
+                console.log(`${name} has been excluded at ${i} grade`);
+                break;
+            }
+            continue;
+        }
+
+        sumGrade += tempGrade;
+        i++;
+    }
+
+    if(!isExcluded) {
+        let avg = sumGrade / 12;
+        console.log(`${name} graduated. Average grade: ${avg.toFixed(2)}`)
+    }     
+}
+
+graduation(["Mimi", 
+"5",
+"6",
+"5",
+"6",
+"5",
+"6",
+"6",
+"2",
+"3"])	
+
