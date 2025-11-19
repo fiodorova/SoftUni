3179e2b2a61d3f3e2fd6e0a1c6e23c2a32265fd6
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-11-19 07:17:26 +0200

    Add files via upload

---
+++ 01. Programming Basics JS/05.2. PB-JS-While-Loop-Exercise/02. Exam Preparation/02-examPreparation.js
@@ -0,0 +1,46 @@
+function examPreparation(input) {
+    let sumBadGrades = Number(input[0]);
+    let index = 1;
+    let command = input[index];
+    let numberOfProblems = 0;
+    let sumGrades = 0;
+    let numberBadGrades = 0;
+ 
+    while(command !== "Enough"){
+        numberOfProblems++;
+        index++;
+
+        let currentGrade = Number(input[index]);
+        sumGrades += currentGrade;
+
+        if(currentGrade <= 4){
+            numberBadGrades++;
+        }
+
+        if(numberBadGrades >= sumBadGrades){
+            console.log(`You need a break, ${numberBadGrades} poor grades.`);
+            break;
+        }
+
+        index++;
+        command = input[index];
+    }
+
+    if(command === "Enough"){
+        let avgGrade = (sumGrades / numberOfProblems).toFixed(2);
+        console.log(`Average score: ${avgGrade}`);
+        console.log(`Number of problems: ${numberOfProblems}`);
+        index -= 2;
+        command = input[index];
+        console.log(`Last problem: ${command}`);
+    }
+}
+
+examPreparation(["2",
+"Income",
+"3",
+"Game Info",
+"6",
+"Best Player",
+"4"])
+
