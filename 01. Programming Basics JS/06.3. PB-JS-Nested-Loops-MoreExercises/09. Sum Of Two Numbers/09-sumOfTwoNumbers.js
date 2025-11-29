87dd3451e3ee01ef4f9b0b1b5f78cc97eb7fbfa4
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-11-29 08:13:08 +0200

    Add files via upload

---
+++ 01. Programming Basics JS/06.3. PB-JS-Nested-Loops-MoreExercises/09. Sum Of Two Numbers/09-sumOfTwoNumbers.js
@@ -0,0 +1,31 @@
+function sumOfTwoNumbers(input){
+        let startNumber = Number(input[0]);
+        let endNumber = Number(input[1]);
+        let magicNumber = Number(input[2]);
+    
+        let isFound = false;
+        let counter = 0
+    
+        for(let x = startNumber; x <= endNumber; x++){
+            for(let y = startNumber; y <= endNumber; y++){
+                let sum = x + y;
+                counter++;
+                if (sum === magicNumber){
+                    isFound = true;
+                    console.log(`Combination N:${counter} (${x} + ${y} = ${magicNumber})`);
+                    break;
+                }
+            }
+    
+            if (isFound){
+                break;
+            }
+        }
+    
+        if(!isFound){
+            console.log(`${counter} combinations - neither equals ${magicNumber}`);
+        }
+    }
+
+
+sumOfTwoNumbers(["1", "10", "5"])
