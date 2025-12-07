e2b15fcaddfe6ed9c1f3dc1acb931e1ee3fcb40f
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-07 09:41:16 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/01.2. Basic Syntax, Conditional Statements and Loops - Exercise/06. Print And Sum/06-printAndSum.js
@@ -0,0 +1,16 @@
+function printAndSum(startNum, endNum){
+    let sum = 0;
+    let sequence = '';
+
+    for(let index = startNum; index <= endNum; index++){
+        sum += index;
+        sequence += index + " ";
+    }
+
+    console.log(sequence);
+    console.log(`Sum: ${sum}`);
+}
+
+printAndSum(5, 10);
+printAndSum(0, 26);
+printAndSum(50, 60);
