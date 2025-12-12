16a414ca253da1be4ca25452ebf91bc2113a1b39
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-12 07:44:56 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/02.1. Data Types and Variables - Lab/02. Concatenate Names/02-concatenateNames.js
@@ -0,0 +1,15 @@
+function concatenateNames(firstName, lastName, delimeter){
+    console.log(firstName + delimeter + lastName);
+}
+
+concatenateNames('John',
+'Smith',
+'->')
+
+concatenateNames('Jan',
+'White',
+'<->')
+
+concatenateNames('Linda',
+'Terry',
+'=>')
