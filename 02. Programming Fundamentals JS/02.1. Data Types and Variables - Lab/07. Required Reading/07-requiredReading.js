2820979117bc3f40f0c7eb9f1f64d20b95eefe92
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-13 06:57:26 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/02.1. Data Types and Variables - Lab/07. Required Reading/07-requiredReading.js
@@ -0,0 +1,14 @@
+function requiredReading(pages, pagesPerHour, days){
+    let hoursPerDay = (pages / pagesPerHour) / days;
+    console.log(hoursPerDay);
+}
+   
+
+requiredReading(212,
+    20 ,
+    2 
+    )
+requiredReading(432,
+    15 ,
+    4 
+    )
