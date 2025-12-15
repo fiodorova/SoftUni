b078f7fd5b24f62182d4056902628c74f2204eaa
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-15 08:44:53 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/02.2. Data Types and Variables - Exercise/02. Chars To String/02-charsToString.js
@@ -0,0 +1,17 @@
+function charsToString(char1, char2, char3){
+    let converted = '' + char1 + char2 + char3;
+    console.log(converted);
+}
+
+charsToString('a',
+'b',
+'c'
+)
+charsToString('%',
+'2',
+'o'
+)
+charsToString('1',
+'5',
+'p'
+)
