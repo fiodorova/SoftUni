b19900b57218e365624d0e053a40e7631efa00ca
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-16 06:38:39 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/02.2. Data Types and Variables - Exercise/07. Lower Or Upper/07-lowerOrUpper.js
@@ -0,0 +1,12 @@
+function lowerOrUpper(char){
+    if(char === char.toUpperCase()){
+        console.log("upper-case");
+    } else {
+        console.log("lower-case");
+    }
+  //  console.log(char === char.toUpperCase() ? "upper-case" : "lower-case")
+}
+
+
+lowerOrUpper('L')
+lowerOrUpper('f')
