2c4bd1ef0467d3c1f69129fd6ee0e50ff12da5fd
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-17 07:24:14 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/02.2. Data Types and Variables - Exercise/09. Gladiator Expenses/09-gladiatorExpenses.js
@@ -0,0 +1,32 @@
+function gladiatorExpenses(
+  lostFights,
+  helmetPrice,
+  swordPrice,
+  shieldPrice,
+  armorPrice
+) {
+  let totalExpenses = 0;
+
+  for (let currFight = 1; currFight <= lostFights; currFight++) {
+    if (currFight % 2 === 0) {
+      totalExpenses += helmetPrice;
+    }
+
+    if (currFight % 3 === 0) {
+      totalExpenses += swordPrice;
+    }
+
+    if (currFight % 6 === 0) {
+      totalExpenses += shieldPrice;
+    }
+
+    if (currFight % 12 === 0) {
+      totalExpenses += armorPrice;
+    }
+  }
+
+  console.log(`Gladiator expenses: ${totalExpenses.toFixed(2)} aureus`);
+}
+
+gladiatorExpenses(7, 2, 3, 4, 5);
+gladiatorExpenses(23, 12.5, 21.5, 40, 200);
