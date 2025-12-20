db99c552ea9346660af316cc9c1b1aaf5b58ac47
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-20 11:02:48 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/03.1. Arrays - Lab/01. Sum First And Last Array Elements/01-sumFirstAndLastArrayElements.js
@@ -0,0 +1,12 @@
+function sumFirstAndLast(numbers){
+    let first = numbers[0];
+    let last = numbers[numbers.length - 1];
+
+    let sum = first + last;
+
+    console.log(sum);
+}
+
+sumFirstAndLast([20, 30, 40])
+sumFirstAndLast([10, 17, 22, 33])
+sumFirstAndLast([11, 58, 69])
