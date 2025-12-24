2843b5c507194980b40b699a4d6b4b514e931a34
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-24 07:25:53 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/03.2. Arrays - Exercise/04. Array Rotation/04-arrayRotation.js
@@ -0,0 +1,13 @@
+function arrayRotation(arr, rot){
+
+    for(let i = 0; i < rot; i++){
+        let current = arr.shift()
+        arr.push(current);
+    }
+
+    console.log(arr.join(' '));
+}
+
+arrayRotation([51, 47, 32, 61, 21], 2)
+arrayRotation([32, 21, 61, 1], 4)
+arrayRotation([2, 4, 15, 31], 5)
