3e92ca98575aaf3b74959f87ada826f9324d55e5
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-29 08:01:37 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/04.1. Functions - Lab/1. Repeat String/1-repeatString.js
@@ -0,0 +1,14 @@
+function repeatString(str, n){
+    let result = '';
+
+    for (let i = 0; i < n; i++) {
+        result += str;        
+    }
+    
+    return result;
+}
+
+console.log(repeatString("abc", 3));
+
+
+repeatString(["String", 2])
