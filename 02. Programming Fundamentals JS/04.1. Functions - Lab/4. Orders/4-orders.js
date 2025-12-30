1ba93796f429f1ac7c0b3c9ce1b4813d0f59f8b4
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-30 07:18:57 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/04.1. Functions - Lab/4. Orders/4-orders.js
@@ -0,0 +1,15 @@
+function orders(product, quantity) {
+    let price = 0;
+    switch(product){
+        case "coffee": price = quantity * 1.5; break;
+        case "water": price = quantity; break;
+        case "coke": price = quantity * 1.4; break;
+        case "snacks": price = quantity * 2; break;
+    }
+
+    console.log(price.toFixed(2));
+}
+
+
+orders("water", 5);
+orders("coffee", 2);
