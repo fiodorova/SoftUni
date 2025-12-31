7ec34107fd516c1b8a6df0565bd01325d26f772b
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2025-12-31 05:46:22 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/04.2. Functions - Exercise/1. Smallest Of Three Numbers/1-smallestOfThreeNumbers.js
@@ -0,0 +1,20 @@
+function smallestOfThreeNumbers(num1, num2, num3) {
+  let smallestNum = function (num1, num2, num3) {
+    let smallestNumber = 0;
+    if (num1 <= num2 && num1 < num3) {
+      smallestNumber = num1;
+    } else if (num2 <= num1 && num2 <= num3) {
+      smallestNumber = num2;
+    } else {
+      smallestNumber = num3;
+    }
+    return smallestNumber;
+  };
+
+  console.log(smallestNum(num1, num2, num3));
+}
+
+smallestOfThreeNumbers(2, 5, 3);
+smallestOfThreeNumbers(600, 342, 123);
+smallestOfThreeNumbers(25, 21, 4);
+smallestOfThreeNumbers(2, 2, 2);
