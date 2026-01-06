8db1536a64be17833fd2789f786d2db8a237445c
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-01-06 06:55:42 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/05.1. Arrays Advanced - Lab/6. Smallest Two Numbers/6-smallestTwoNumbers.js
@@ -0,0 +1,8 @@
+function smallestTwoNumbers(nums){
+    return nums.sort((a, b) => a - b).slice(0, 2).join(' ');
+}
+
+console.log(smallestTwoNumbers([30, 15, 50, 5]));
+console.log(smallestTwoNumbers([3, 0, 10, 4, 7, 3]));
+
+
