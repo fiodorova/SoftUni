3bf4a6e52210d9d66f9069ffd3a0e10dcf6b20f0
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-01-06 06:52:55 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/05.1. Arrays Advanced - Lab/5. Process Odd Numbers/5-processOddNumbers.js
@@ -0,0 +1,10 @@
+function processOddNumbers(arr){
+     console.log(arr
+        .filter((x, i) => i % 2 == 1)
+        .map(x => x * 2)
+        .reverse()
+        .join(' '));
+}
+
+processOddNumbers([10, 15, 20, 25])
+processOddNumbers([3, 0, 10, 4, 7, 3])
