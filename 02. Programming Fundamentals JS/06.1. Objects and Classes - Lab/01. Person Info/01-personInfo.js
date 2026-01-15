1735e3a1ac7f26aa6c66deece159f7c519855fff
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-01-15 07:10:13 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/06.1. Objects and Classes - Lab/01. Person Info/01-personInfo.js
@@ -0,0 +1,24 @@
+function personInfo(firstName, lastName, age){
+ 
+    let result = {
+        firstName,
+        lastName,
+        age
+    };
+
+    return result;
+}
+
+console.log(personInfo("Peter", 
+"Pan",
+"20"
+));
+
+personInfo("Peter", 
+"Pan",
+"20"
+)
+personInfo("George", 
+"Smith",
+"18"
+)
