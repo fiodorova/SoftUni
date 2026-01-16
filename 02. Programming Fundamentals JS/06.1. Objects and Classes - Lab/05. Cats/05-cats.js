abaef94413d4116396d154c0b9a15443900ae45b
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-01-16 07:33:09 +0200

    Add files via upload

---
+++ 02. Programming Fundamentals JS/06.1. Objects and Classes - Lab/05. Cats/05-cats.js
@@ -0,0 +1,28 @@
+function cats(parseCats){
+
+    class Cat {
+        constructor(name, age){
+            this.name = name;
+            this.age = age;
+        }
+
+        meow(){
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    let cats = []
+
+    for(let catAsString of parseCats){
+        let tokens = catAsString.split(' ')
+        let cat = new Cat(tokens[0], Number(tokens[1]))
+        cats.push(cat)
+    }
+
+    for(let cat of cats){
+        cat.meow()
+    }
+}
+
+cats(['Mellow 2', 'Tom 5'])
+cats(['Candy 1', 'Poppy 3', 'Nyx 2'])
