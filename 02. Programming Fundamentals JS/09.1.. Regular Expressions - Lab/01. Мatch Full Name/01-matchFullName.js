08ac1812d20bc08d3d0754e7ccac280f20139512
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-02-01 09:12:19 +0200

    Update 01-matchFullName.js

--- 02. Programming Fundamentals JS/09.1.. Regular Expressions - Lab/01. Мatch Full Name/01-matchFullName.js
+++ 02. Programming Fundamentals JS/09.1.. Regular Expressions - Lab/01. Мatch Full Name/01-matchFullName.js
@@ -1,1 +1,18 @@
+function fullName(input) {
+  let pattern = /\b[A-Z][a-z]{1,} [A-Z][a-z]{1,}\b/g;
 
+  let validNames = [];
+  let match = pattern.exec(input);
+
+  while (match != null) {
+    validNames.push(match[0]);
+
+    match = pattern.exec(input);
+  }
+
+  console.log(validNames.join(" "));
+}
+
+fullName(
+  "Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov"
+);
