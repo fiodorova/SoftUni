232c078071b813b9cdcc3dd8789baf242c40ff89
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-02-08 08:50:44 +0200

    Add files via upload

---
+++ 03. JS-Advanced/01.2. Syntax, Functions and Statements - Exercise/03. Same Numbers/03-sameNumbers.js
@@ -0,0 +1,21 @@
+function sameNumbers(number){
+    let input = String(number);
+    let isSame = true;
+    let sum = 0;
+
+    let firstDigit = input[0];
+
+    for(let i = 0; i < input.length; i++){
+        let currentDigit = Number(input[i])
+        sum += currentDigit;
+        if(firstDigit != currentDigit){
+            isSame = false;
+        }
+    }
+
+    console.log(isSame);
+    console.log(sum);
+}
+
+sameNumbers(2222222)
+sameNumbers(1234)
