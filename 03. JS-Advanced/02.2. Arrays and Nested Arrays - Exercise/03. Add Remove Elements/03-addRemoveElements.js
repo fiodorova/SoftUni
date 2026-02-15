9bdf3fc955459f9345129148ffd65ccc73bf2ff9
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-02-15 09:55:22 +0200

    Add files via upload

---
+++ 03. JS-Advanced/02.2. Arrays and Nested Arrays - Exercise/03. Add Remove Elements/03-addRemoveElements.js
@@ -0,0 +1,36 @@
+function addRemoveElements(arr){
+    let newArr = [];
+    let num = 0;
+
+    for(let index = 0; index <= arr.length; index++){
+        num++;
+        let command = arr[index];
+        if(command == 'add'){
+            newArr.push(num);
+        } else if(command == 'remove'){
+            newArr.pop()
+        }
+    }
+
+    if(newArr.length == 0){
+        console.log('Empty');
+    } else {
+        console.log(newArr.join('\n'));
+    }    
+}
+
+addRemoveElements(['add', 
+'add', 
+'add', 
+'add']
+);
+addRemoveElements(['add', 
+'add', 
+'remove', 
+'add', 
+'add']
+);
+addRemoveElements(['remove', 
+'remove', 
+'remove']
+);
