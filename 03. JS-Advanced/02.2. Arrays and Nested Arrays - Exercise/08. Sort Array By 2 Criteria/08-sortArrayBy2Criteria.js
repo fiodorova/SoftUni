fe61a858c7452227aea6c23dba285c3bb3a4d0a7
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-02-17 07:38:01 +0200

    Add files via upload

---
+++ 03. JS-Advanced/02.2. Arrays and Nested Arrays - Exercise/08. Sort Array By 2 Criteria/08-sortArrayBy2Criteria.js
@@ -0,0 +1,28 @@
+function sortArrayBy2Criteria(arr){
+    arr.sort(twoCriteriaSort);
+    return arr.join('\n')
+
+    function twoCriteriaSort(current, next){
+        if(current.length === next.length){
+            return current.localeCompare(next);
+        }
+
+        return current.length - next.length
+    }
+}
+
+console.log(sortArrayBy2Criteria(['alpha', 
+'beta', 
+'gamma']
+));
+console.log(sortArrayBy2Criteria(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']
+));
+console.log(sortArrayBy2Criteria(['test', 
+'Deny', 
+'omen', 
+'Default']
+));
