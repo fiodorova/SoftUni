95be6b846da5841fc4878d50034290d5b1527729
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-02-21 07:17:23 +0200

    Add files via upload

---
+++ 03. JS-Advanced/03.2. Objects and Composition - Exercise/02. Construction Crew/02-constructionCrew.js
@@ -0,0 +1,25 @@
+function constructionCrew(obj){
+
+    if(obj.dizziness){
+        obj.levelOfHydrated += 0.1 * obj.experience * obj.weight;
+        obj.dizziness = false;
+    }
+
+    return obj;
+}
+
+constructionCrew({ weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true }
+  );
+constructionCrew({ weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true }
+  );
+constructionCrew({ weight: 95,
+    experience: 3,
+    levelOfHydrated: 0,
+    dizziness: false }
+  );
