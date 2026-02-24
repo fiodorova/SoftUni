5603b004e169067ead3aa0749f0c0aeabaeef01c
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-02-24 06:04:28 +0200

    Add files via upload

---
+++ 03. JS-Advanced/04.1. DOM-Introduction - Lab/01. Edit Element/EditElement.js
@@ -0,0 +1,10 @@
+function editElement(element, match, replacer){
+    const text = element.textContent;
+
+    const pattern = new RegExp(match, 'g')
+    const result = text.replace(pattern, replacer);
+
+    //const result = text.split(match).join(replacer);
+    element.textContent = result;
+}
+
