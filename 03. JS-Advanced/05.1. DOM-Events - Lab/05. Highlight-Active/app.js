edb84e4f2f60fead3a23cd648c4128d7fac3773e
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-03-03 07:19:10 +0200

    Add files via upload

---
+++ 03. JS-Advanced/05.1. DOM-Events - Lab/05. Highlight-Active/app.js
@@ -0,0 +1,24 @@
+function focused() {
+    Array.from(document.querySelectorAll('input'))
+    .forEach(i => {
+        i.addEventListener('focus', onFocus);
+        i.addEventListener('blur', onBlur);
+    });
+
+    function onFocus(ev){
+        ev.target.parentElement.className = 'focused';
+    }
+
+    function onBlur(ev){
+        ev.target.parentElement.className = '';
+    }
+
+  /*  function onFocus(ev){
+        ev.target.parentElement.classList.add('focused');
+    }
+
+    function onBlur(ev){
+        ev.target.parentElement.classList.remove('focused');
+    }
+    */
+}
