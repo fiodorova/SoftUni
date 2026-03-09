36f5d66ca511cb3ecdde9c0f7a8a5f4544e96da6
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-03-09 07:39:41 +0200

    Add files via upload

---
+++ 03. JS-Advanced/06.2. Advanced-Functions-Exercise/02. Argument Info/02-argumentInfo.js
@@ -0,0 +1,25 @@
+function argumentInfo() {
+  const params = Array.from(arguments);
+
+  const types = {};
+
+  for (let arg of params) {
+    const type = typeof arg;
+    console.log(`${type}: ${arg}`);
+
+    if (types[type] == undefined) {
+      types[type] = 0;
+    }
+    types[type]++;
+  }
+
+  const result = Object.entries(types).sort((a, b) => b[1] - a[1]);
+
+  for (let [type, count] of result) {
+    console.log(`${type} = ${count}`);
+  }
+}
+
+argumentInfo("cat", 42, function () {
+  console.log("Hello world!");
+});
