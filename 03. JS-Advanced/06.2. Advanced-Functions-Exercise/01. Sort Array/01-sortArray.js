80028df42d92127d1fe9740720e10c5062808af6
Silvia Fiodorova <102142352+fiodorova@users.noreply.github.com>
2026-03-09 06:53:03 +0200

    Add files via upload

---
+++ 03. JS-Advanced/06.2. Advanced-Functions-Exercise/01. Sort Array/01-sortArray.js
@@ -0,0 +1,13 @@
+function sortArray(arr, type) {
+  const methods = {
+    asc: (a, b) => a - b,
+    desc: (a, b) => b - a,
+  };
+
+  arr.sort(methods[type]);
+
+  return arr;
+}
+
+sortArray([14, 7, 17, 6, 8], "asc");
+sortArray([14, 7, 17, 6, 8], "desc");
